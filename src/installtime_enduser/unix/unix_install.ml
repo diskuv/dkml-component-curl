15df5fa330efcc4a78e569e53ae9bdcc895dba13
Jonah Beckford <9566106-jonahbeckford@users.noreply.gitlab.com>
2022-04-26 01:38:50 -0700

    Correct args to symlink

--- src/installtime_enduser/unix/unix_install.ml
+++ src/installtime_enduser/unix/unix_install.ml
@@ -10,13 +10,16 @@ let () =
           "Destination path for a symlink to the curl on the PATH" );
       ]
       anon_fun "Install curl on Unix");
+  if !opt_target = "" then (
+    prerr_endline "FATAL: The -target PATH option is required.";
+    exit 1);
   (* Installation: Make a symlink *)
   let ( let* ) = Rresult.R.( >>= ) in
   let open Bos in
   let install_sequence =
-    let* target_curl = Fpath.of_string !opt_target in
-    let* src_curl = OS.Cmd.get_tool (Cmd.v "curl") in
-    let* _was_created = OS.Dir.create ~mode:0o750 (Fpath.parent target_curl) in
-    OS.Path.symlink ~target:target_curl src_curl
+    let* link_curl = Fpath.of_string !opt_target in
+    let* target = OS.Cmd.get_tool (Cmd.v "curl") in
+    let* _was_created = OS.Dir.create ~mode:0o750 (Fpath.parent link_curl) in
+    OS.Path.symlink ~target link_curl
   in
   Rresult.R.error_msg_to_invalid_arg install_sequence
